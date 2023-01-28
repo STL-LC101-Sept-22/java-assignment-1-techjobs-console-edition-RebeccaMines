17496b498765023d19f5134a162cdee7773e32ad
RebeccaMines <becca.mines22297@gmail.com>
2023-01-28 17:47:30 -0600

    Code works as intended, not sure if tests will pass

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -105,6 +105,7 @@ public class TechJobs {
                 }
             }
 
+
             // Validate user's input
             if (choiceIdx < 0 || choiceIdx >= choiceKeys.length) {
                 System.out.println("Invalid choice. Try again.");
